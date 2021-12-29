523d642e97dd30524ec68e3195602ce4197fe49c
Raja kopparthi <ayyanababu@gmail.com>
2021-12-29 13:15:49 +0530

    added component to testing

--- App.js
+++ App.js
@@ -1,72 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  NCButton,
+  getRouteConfig,
+  renderComponent,
+} from 'react-native-nc-components-levelone';
 
-import RNNinjaComponentsOneModule, {NCButton} from 'rn-ninja-components-one';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const config = {
+  type: 'tab',
+  screens_config: [
+    {
+      title: 'Home Screen',
+      component: () => renderComponent({name: 'Home Screen body'}),
+    },
+    {
+      title: 'Details Screen',
+      component: () => renderComponent({name: 'Details Screen body'}),
+    },
+  ],
+  innerNavigation: false,
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NCButton />
-    </SafeAreaView>
-  );
+const App = () => {
+  return getRouteConfig(config);
 };
 
 const styles = StyleSheet.create({
